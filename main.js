65431eba53e3fef4217b5ef711be7c88ab25d479
eliaraguso <elia.raguso@gmail.com>
2021-09-19 11:15:23 +0200

    aggiunti metodi in vue per cambiare immagini e cicli in html per far variare i bottoni blu allo scorrere delle imagini

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-var app = new Vue({
+const app = new Vue({
     el: '#root',
     data: {
-      
+        images : [
+            "https://mdbootstrap.com/img/Photos/Slides/img%20(88).jpg",
+            "https://mdbootstrap.com/img/Photos/Slides/img%20(121).jpg",
+            "https://mdbootstrap.com/img/Photos/Slides/img%20(31).jpg"
+        ],
+        imageIndex: 0
     },
     methods: {
+        nextImage : function() {
+            this.imageIndex++;
+            if (this.imageIndex >= this.images.length) {
+                this.imageIndex = 0;
+            }
+        },
+        prevImage : function() {
+            this.imageIndex--;
+            if (this.imageIndex < 0) {
+                this.imageIndex = this.images.length - 1;
+            } 
 
+        }
+        
     }
   });
